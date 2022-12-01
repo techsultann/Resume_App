f165dcb2e44f41e09ad35709b07622cfecb5e78d
Techsultan <salamsultan09@gmail.com>
2022-12-01 11:31:46 +0100

    UI improvement

--- app/src/main/java/com/example/resumeapp/MainActivity.kt
+++ app/src/main/java/com/example/resumeapp/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.resumeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.resumeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var appBarConfiguration:AppBarConfiguration
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +31,29 @@ class MainActivity : AppCompatActivity() {
         val navController = host.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBar(navController, appBarConfiguration)
     }
+
+    // It allows the NavigationUi to decide what label to show in the action bar
+    // It also determine whether to show the up arrow or drawer menu icon
+    private fun setupActionBar(navController: NavController,
+                               appBarConfig: AppBarConfiguration) {
+
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
